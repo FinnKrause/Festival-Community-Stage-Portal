ad4c3831f3734771a5bc6a4d8b770a6e19229f06
FinnKrause <finn_krause@gmx.de>
2026-03-20 01:35:02 +0100

    Updates

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["songreq.finnkrause.com", "192.168.178.81:3000"],
+  allowedDevOrigins: [
+    "songreq.finnkrause.com",
+    "stage.fsi-wiso.de",
+    "songreq.fsi-wiso.de",
+    "192.168.178.81:3000",
+  ],
   async redirects() {
     return [{ source: "/", destination: "/songreq", permanent: true }];
   },
